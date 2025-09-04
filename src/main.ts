1d956ee252cc115f29ff7d2a455d12544ca12108
jesus fermin de paz eras <55634393+xxxferminxxx123@users.noreply.github.com>
2025-09-04 17:07:25 +0000

    feat(main): Agregar autenticacion JWT

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response, NextFunction } from "express";
 import { CartillaContainer } from "./lib/Shared/Cartilla/Cartilla/Container/CartillaContainer";
 import { CartillaRouter } from "./lib/Shared/Cartilla/Cartilla/Router/CartillaRouter";
+import { PostsRouter } from "./lib/Cartilla/Posts/PostsRouter";
+import jwt from "jsonwebtoken";
 
 (async () => {
 
   const app = express();
-  
+  const JWT = "221ce4f433152e10922a4771bea3b00e5abce3585a949892e66108ebb4bbbe1300ae09d3a2f6bafd650a80645b4ba4ec8904f6e2a7eb429d59f71c017e790c7e";
   app.use(express.json());
 
   app.use((req, res, next) => {
@@ -17,8 +19,21 @@ import { CartillaRouter } from "./lib/Shared/Cartilla/Cartilla/Router/CartillaRo
   });
 
   const serviceCartilla = await CartillaContainer();
-
+  
+  app.use("/posts", PostsRouter());
   app.use("/cartilla", CartillaRouter(serviceCartilla.cartilla));
+  app.post("/login", (req,res)=>{
+    const username= req.body.username;
+    const password= req.body.password;
+    const user = {
+      username: username
+      ,password: password
+    }
+    if()
+    const accesToken = jwt.sign(user,JWT)
+
+    res.json({accesToken:accesToken})
+    })
 
   app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
